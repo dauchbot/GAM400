03c1fbc7571885bfb55734163db28ca1f202d8c3
Dauchot <dauch.bot@gmail.com>
2015-11-12 17:43:39 -0800

    code for movement stuff, will need to get rid of capsule and replace with sphere

--- Source/FernGullyLike/FernGullyLikeCharacter.cpp
+++ Source/FernGullyLike/FernGullyLikeCharacter.cpp
@@ -2,9 +2,20 @@
 
 #include "FernGullyLike.h"
 #include "FernGullyLikeCharacter.h"
+#include "FernGullyLikeProjectile.h"
+#include "TimerManager.h"
+
+const FName AFernGullyLikeCharacter::MoveForwardBinding("MoveForward");
+const FName AFernGullyLikeCharacter::MoveRightBinding("MoveRight");
+const FName AFernGullyLikeCharacter::FireForwardBinding("FireForward");
+const FName AFernGullyLikeCharacter::FireRightBinding("FireRight");
 
 AFernGullyLikeCharacter::AFernGullyLikeCharacter()
 {
+  // Cache our sound effect
+  //static ConstructorHelpers::FObjectFinder<USoundBase> FireAudio(TEXT("/Game/TwinStick/Audio/TwinStickFire.TwinStickFire"));
+  //FireSound = FireAudio.Object;
+
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
@@ -27,15 +38,19 @@ AFernGullyLikeCharacter::AFernGullyLikeCharacter()
 	SideViewCameraComponent->AttachTo(CameraBoom, USpringArmComponent::SocketName);
 	SideViewCameraComponent->bUsePawnControlRotation = false; // We don't want the controller rotating the camera
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Face in the direction we are moving..
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f); // ...at this rotation rate
-	GetCharacterMovement()->GravityScale = 2.f;
-	GetCharacterMovement()->AirControl = 0.80f;
-	GetCharacterMovement()->JumpZVelocity = 1000.f;
-	GetCharacterMovement()->GroundFriction = 3.f;
-	GetCharacterMovement()->MaxWalkSpeed = 600.f;
-	GetCharacterMovement()->MaxFlySpeed = 600.f;
+
+
+  GetCharacterMovement()->GravityScale = 0.f;
+  GetCharacterMovement()->AirControl = 1.f;
+  GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+  GetCharacterMovement()->SetGroundMovementMode(MOVE_Flying);
+
+  // Movement
+  MoveSpeed = 1000.0f;
+  // Weapon
+  GunOffset = FVector(0.f, 90.f, 0.f);
+  FireRate = 0.1f;
+  bCanFire = true;
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
@@ -43,32 +58,87 @@ AFernGullyLikeCharacter::AFernGullyLikeCharacter()
 
 //////////////////////////////////////////////////////////////////////////
 // Input
-
 void AFernGullyLikeCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
-	// set up gameplay key bindings
-	InputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	InputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
-	InputComponent->BindAxis("MoveRight", this, &AFernGullyLikeCharacter::MoveRight);
+  check(InputComponent);
 
-	InputComponent->BindTouch(IE_Pressed, this, &AFernGullyLikeCharacter::TouchStarted);
-	InputComponent->BindTouch(IE_Released, this, &AFernGullyLikeCharacter::TouchStopped);
+  // set up gameplay key bindings
+  InputComponent->BindAxis(MoveForwardBinding);
+  InputComponent->BindAxis(MoveRightBinding);
+  InputComponent->BindAxis(FireForwardBinding);
+  InputComponent->BindAxis(FireRightBinding);
 }
 
-void AFernGullyLikeCharacter::MoveRight(float Value)
+void AFernGullyLikeCharacter::Tick(float DeltaSeconds)
 {
-	// add movement in that direction
-	AddMovementInput(FVector(0.f,-1.f,0.f), Value);
+  // Find movement direction
+  const float ForwardValue = GetInputAxisValue(MoveForwardBinding);
+  const float RightValue = GetInputAxisValue(MoveRightBinding);
+
+  // Clamp max size so that (X=1, Y=1) doesn't cause faster movement in diagonal directions
+  const FVector MoveDirection = FVector(0.f, -RightValue, ForwardValue).GetClampedToMaxSize(1.0f);
+
+  // Calculate  movement
+  Movement = MoveDirection * MoveSpeed * DeltaSeconds;
+
+  // If non-zero size, move this actor
+  if (Movement.SizeSquared() > 0.0f)
+  {
+    const FRotator NewRotation = Movement.Rotation();
+    FHitResult Hit(1.f);
+    RootComponent->MoveComponent(Movement, NewRotation, true, &Hit);
+
+    if (Hit.IsValidBlockingHit())
+    {
+      const FVector Normal2D = Hit.Normal.GetSafeNormal2D();
+      const FVector Deflection = FVector::VectorPlaneProject(Movement, Normal2D) * (1.f - Hit.Time);
+      RootComponent->MoveComponent(Deflection, NewRotation, true);
+    }
+  }
+
+  // Create fire direction vector
+  const float FireForwardValue = GetInputAxisValue(FireForwardBinding);
+  const float FireRightValue = GetInputAxisValue(FireRightBinding);
+  const FVector FireDirection = FVector(0.f, FireRightValue, FireForwardValue);
+
+  // Try and fire a shot
+  FireShot(FireDirection);
 }
 
-void AFernGullyLikeCharacter::TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location)
+void AFernGullyLikeCharacter::FireShot(FVector FireDirection)
 {
-	// jump on any touch
-	Jump();
+  // If we it's ok to fire again
+  if (bCanFire == true)
+  {
+    // If we are pressing fire stick in a direction
+    if (FireDirection.SizeSquared() > 0.0f)
+    {
+      const FRotator FireRotation = FireDirection.Rotation();
+      // Spawn projectile at an offset from this pawn
+      const FVector SpawnLocation = GetActorLocation() + FireRotation.RotateVector(GunOffset);
+
+      UWorld* const World = GetWorld();
+      if (World != NULL)
+      {
+        // spawn the projectile
+        World->SpawnActor<AFernGullyLikeProjectile>(SpawnLocation, FireRotation);
+      }
+
+      bCanFire = false;
+      World->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &AFernGullyLikeCharacter::ShotTimerExpired, FireRate);
+
+      // try and play the sound if specified
+      if (FireSound != nullptr)
+      {
+        UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+      }
+
+      bCanFire = false;
+    }
+  }
 }
 
-void AFernGullyLikeCharacter::TouchStopped(const ETouchIndex::Type FingerIndex, const FVector Location)
+void AFernGullyLikeCharacter::ShotTimerExpired()
 {
-	StopJumping();
+  bCanFire = true;
 }
-
--- Source/FernGullyLike/FernGullyLikeCharacter.h
+++ Source/FernGullyLike/FernGullyLikeCharacter.h
@@ -18,18 +18,50 @@ class AFernGullyLikeCharacter : public ACharacter
 
 protected:
 
-	/** Called for side to side input */
-	void MoveRight(float Val);
+  /** Offset from the ships location to spawn projectiles */
+  UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+    FVector GunOffset;
 
-	/** Handle touch inputs. */
-	void TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location);
+  /** Offset from the ships location to spawn projectiles */
+  UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+    FVector Movement;
 
-	/** Handle touch stop event. */
-	void TouchStopped(const ETouchIndex::Type FingerIndex, const FVector Location);
+  /* How fast the weapon will fire */
+  UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+    float FireRate;
 
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
-	// End of APawn interface
+  /* The speed our ship moves around the level */
+  UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+    float MoveSpeed;
+
+  /** Sound to play each time we fire */
+  UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
+  class USoundBase* FireSound;
+
+  // Begin Actor Interface
+  virtual void Tick(float DeltaSeconds) override;
+  virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+  // End Actor Interface
+
+  /* Fire a shot in the specified direction */
+  void FireShot(FVector FireDirection);
+
+  /* Handler for the fire timer expiry */
+  void ShotTimerExpired();
+
+  // Static names for axis bindings
+  static const FName MoveForwardBinding;
+  static const FName MoveRightBinding;
+  static const FName FireForwardBinding;
+  static const FName FireRightBinding;
+
+private:
+
+  /* Flag to control firing  */
+  uint32 bCanFire : 1;
+
+  /** Handle for efficient management of ShotTimerExpired timer */
+  FTimerHandle TimerHandle_ShotTimerExpired;
 
 
 public:
